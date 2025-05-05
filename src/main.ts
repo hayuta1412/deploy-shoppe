ad0e50536e1f107ac79c2d2dc834d5ec79f4ba97
hayuta14 <maiducanh1412@gmail.com>
2025-05-05 17:31:18 +0700

    update

--- src/main.ts
+++ src/main.ts
@@ -7,28 +7,31 @@ import { HttpAdapterHost } from '@nestjs/core';
 import { GlobalExceptionFilter } from './common/exceptions/global-exception.filter';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
-const expressApp = express();
-
-export const createNestServer = async () => {
+async function bootstrap() {
+  const expressApp = express();
   const app = await NestFactory.create(AppModule, new ExpressAdapter(expressApp));
 
+  // Enable CORS
   app.enableCors({
     origin: '*',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
 
+  // Validation + Global error handling
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
   app.useGlobalFilters(new GlobalExceptionFilter(app.get(HttpAdapterHost)));
+
+  // Global API prefix
   app.setGlobalPrefix(process.env.API_PREFIX || 'api/v1');
 
+  // Swagger
   const config = new DocumentBuilder()
     .setTitle('Shoppe example')
     .setDescription('The shop API description')
     .setVersion('1.0')
     .addBearerAuth()
     .build();
-
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document, {
     swaggerOptions: {
@@ -36,6 +39,10 @@ export const createNestServer = async () => {
     },
   });
 
-  await app.init();
-  return expressApp;
-};
+  // Start server
+  const port = process.env.PORT || 3000;
+  await app.listen(port);
+  console.log(`🚀 Server listening on http://localhost:${port}`);
+}
+
+bootstrap();
