6770ab5e551ce8c9a9ed0d22c7e0a6efd10809a1
hayuta14 <maiducanh1412@gmail.com>
2025-05-05 17:44:42 +0700

    update

--- src/main.ts
+++ src/main.ts
@@ -1,38 +1,31 @@
-import { NestFactory } from '@nestjs/core';
+import { HttpAdapterHost, NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
-import { HttpAdapterHost } from '@nestjs/core';
 import { GlobalExceptionFilter } from './common/exceptions/global-exception.filter';
-import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
   app.enableCors({
     origin: '*',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    credentials: true,
+    credentials: true, // Cho phép gửi cookie
   });
-
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
   app.useGlobalFilters(new GlobalExceptionFilter(app.get(HttpAdapterHost)));
-
   app.setGlobalPrefix(process.env.API_PREFIX || 'api/v1');
-
   const config = new DocumentBuilder()
     .setTitle('Shoppe example')
     .setDescription('The shop API description')
     .setVersion('1.0')
     .addBearerAuth()
     .build();
-  const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document, {
-    swaggerOptions: { persistAuthorization: true },
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory, {
+    swaggerOptions: {
+      persistAuthorization: true,
+    },
   });
-
-  const port = process.env.PORT || 4000;
-  await app.listen(port);
-  console.log(`🚀 Server listening on http://localhost:${port}`);
+  await app.listen(process.env.PORT ?? 4000);
 }
-
 bootstrap();
